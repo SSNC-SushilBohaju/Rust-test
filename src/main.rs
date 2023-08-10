311430f6b44451a45717c6aba7676d9154734909
SushilBohaju <112843183+SSNC-SushilBohaju@users.noreply.github.com>
2023-08-10 17:22:48 +0900

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+mod aws_connection;
+use aws_connection::bootstrap;
+use constant::ISSUE_AND_ASSOCIATE_DEVICE_ID_URL;
+
+#[tokio::main]
+async fn main() {
+    let url:&str =ISSUE_AND_ASSOCIATE_DEVICE_ID_URL;
+    bootstrap::auth_download_file(url).await;
+
+}
+
